75948299357bb6fbb2c66050676fd4de3c2ddf80
Marcello Pontes <balaum@gmail.com>
2015-05-07 13:12:27 +0200

    First commit.
    There are two main objects here.
     1. The object that has the capacity to expose urls to unauthenticated
    users into the platform - needed for showing images, css, js from the
    PUCLogin page;
     2. The object that is a template factory for processing plugin
    templates contextualized with server-side (Java) variables. This bean
    is going to be used from PUCLogin to process its template.

---
+++ src/com/oncase/tapa/engine/TapaTemplateFactory.java
@@ -0,0 +1,107 @@
+package com.oncase.tapa.engine;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Map;
+
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+
+import com.mitchellbosecke.pebble.PebbleEngine;
+import com.mitchellbosecke.pebble.error.PebbleException;
+import com.mitchellbosecke.pebble.loader.FileLoader;
+import com.mitchellbosecke.pebble.template.PebbleTemplate;
+
+/**
+ * The engine that is reponsible for compiling templates for 
+ * <a target="_blank" href="http://github.com/marpontes/tapa">Tapa Plugin.</a>
+ * <p>This class will be instantiated as a spring-bean from plugin.spring.xml
+ * <p>The way to retrieve it is by calling the method getBean() from 
+ * IPluginManager.<p>
+ * Once the object is retrieved, it can be operated using reflection.
+ * This way, you won't need to import its classes to the code.
+ * 
+ * @author <a target="_blank" href="http://about.me/marpontes">
+ * {@literal @}marpontes</a>
+ */
+public class TapaTemplateFactory {
+	
+	private FileLoader loader;
+	private PebbleEngine engine;
+	
+	/**
+	 * The constructor receives no parameter, once the factory is supposed to know
+	 * by configuration, where are the templates and what is their suffix .
+	 */
+	public TapaTemplateFactory()  {
+		
+		loader = new FileLoader();
+		loader.setSuffix(getTemplatesSuffix());
+		loader.setPrefix(getTemplatesPath());
+		
+		engine = new PebbleEngine(loader);
+		
+	}
+	
+	/**
+	 * This method is used to retrieve a String that contains the required template.
+	 * However, by providing no context, it compiles with no variables.
+	 * 
+	 * @param templateName 	The template name - usually the filename without extension
+	 * @return				The template without any variables applied
+	 * @throws IOException 
+	 * @throws PebbleException 
+	 */
+	public String getTemplate(String templateName) throws PebbleException, IOException {
+		
+		return getTemplate(templateName,null);
+		
+	}
+	
+	/**
+	 * This method is used to retrieve a String that contains the required template.
+	 * When supplied a map of variables and their values, it returns the compiled template.
+	 * 
+	 * @param templateName 	The template name - usually the filename without extension
+	 * @param context		A map of variables and their values for compilation
+	 * @return				The compiled template with all variables considered
+	 * @throws PebbleException 
+	 * @throws IOException 
+	 */
+	public String getTemplate(String templateName, Map<String, Object> context) 
+			throws PebbleException, IOException{
+	
+		PebbleTemplate template = engine.getTemplate(templateName);
+		Writer writer = new StringWriter();
+		
+		if(context==null){
+			template.evaluate(writer);
+		}else{
+			template.evaluate(writer, context);
+		}
+		
+		return writer.toString();
+		
+	}
+	
+	/**
+	 * @return 	The path to the templates
+	 * @todo	Resolve the dependency to PentahoSystem
+	 */
+	private String getTemplatesPath(){
+		
+		return PentahoSystem.getApplicationContext().getSolutionPath("") 
+				+ "/system/tapa/resources/templates/";
+		
+	}
+	
+	/**
+	 * @return The templates suffix - files extension
+	 */
+	private String getTemplatesSuffix(){
+		
+		return ".html";
+		
+	}
+	
+}
